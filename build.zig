70d08f4b52bc641de0101458b2a07600b6cce8b4
sea-grass <dev.seagrass@gmail.com>
2026-04-12 18:57:15 -0400

    Upgrade to Zig 0.16.0-dev.3059+42e33db9d

--- build.zig
+++ build.zig
@@ -10,21 +10,22 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const lib = b.addLibrary(.{
-        .name = "quickjs",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-        }),
-        .linkage = .static,
+    const module = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
     });
 
-    lib.linkLibC();
-
-    lib.addCSourceFiles(.{
+    module.addCSourceFiles(.{
         .root = upstream.path("."),
         .files = source_files,
         .flags = compile_flags,
+    });
+
+    const lib = b.addLibrary(.{
+        .name = "quickjs",
+        .root_module = module,
+        .linkage = .static,
     });
 
     lib.installHeadersDirectory(
